1350da8420b394835da905240a6c7237582552b3
Oleksandr Menchynskyi <the.mngs@yahoo.com>
2020-05-03 15:21:10 +0300

    Add page headings

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,29 @@
 import React from 'react';
 import { JokesProvider } from './contexts';
 import { JokeList, FavouriteList } from './components';
-import { MainContainer, AsideContainer } from './AppStyled';
+import {
+  MainContainer,
+  AsideContainer,
+  PageTitle,
+  AsideTitle,
+  SectionStyled,
+  SectionTitle,
+  TextContainer,
+} from './AppStyled';
 
 export const App: React.FC = () => {
   return (
     <JokesProvider>
       <MainContainer>
-        <JokeList />
+        <PageTitle>MSI 2020</PageTitle>
+        <SectionStyled>
+          <SectionTitle>Hey!</SectionTitle>
+          <TextContainer>Let’s try to find a joke for you:</TextContainer>
+          <JokeList />
+        </SectionStyled>
       </MainContainer>
       <AsideContainer>
+        <AsideTitle>Favourite</AsideTitle>
         <FavouriteList />
       </AsideContainer>
     </JokesProvider>
--- src/AppStyled.ts
+++ src/AppStyled.ts
@@ -6,12 +6,45 @@ export const MainContainer = styled.main`
   margin-left: 140px;
 `;
 
+export const PageTitle = styled.h1`
+  margin: 40px 0 0 0;
+  font-weight: bold;
+  font-size: ${({ theme }) => theme.fontSize.heading.small};
+  line-height: 28px;
+`;
+
 export const AsideContainer = styled.aside`
   position: fixed;
   top: 0;
   right: 0;
   height: 100%;
   width: 480px;
-  padding: 40px;
+  padding: 20px 40px;
   background-color: ${({ theme }) => theme.colors.background.secondary};
+`;
+
+export const AsideTitle = styled.h2`
+  margin: 20px 0 20px 0;
+  font-weight: 500;
+  font-size: ${({ theme }) => theme.fontSize.heading.small};
+  line-height: 28px;
+  color: ${({ theme }) => theme.colors.text.secondary};
+`;
+
+export const SectionStyled = styled.section`
+  margin-top: 78px;
+`;
+
+export const SectionTitle = styled.h2`
+  margin: 0;
+  font-weight: bold;
+  font-size: ${({ theme }) => theme.fontSize.heading.large};
+  line-height: 44px;
+`;
+
+export const TextContainer = styled.p`
+  margin: 0 0 43px 0;
+  font-weight: 500;
+  font-size: ${({ theme }) => theme.fontSize.heading.regular};
+  line-height: 44px;
 `;
